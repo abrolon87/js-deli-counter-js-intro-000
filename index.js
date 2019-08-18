d4ad87f82507fb81b90bdb46445e3b2cf3450107
Amanda R <abrolon87@gmail.com>
2019-08-18 03:56:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ function nowServing(katzDeliLine) {
     return "Currently serving " + firstInLine + ".";
   }
 }
+//passed
+function currentLine(line) {
+  var lineNum = [];
+  if (line.length == 0) {
+    return "The line is currently empty.";
+  }
+  for (var i = 0; i < line.length; i++) {
+  lineNum.push(' ${i+1}. ${line[i]}');
+}
+  return "The line is currently:" + lineNum;
+}
