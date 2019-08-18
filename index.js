e71d4b88246d248495f4237feb04bc20b40ac68a
Amanda R <abrolon87@gmail.com>
2019-08-18 03:49:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return "Welcome, " + name + ". You are number " + (katzDeliLine.indexOf(name) + 1) + " in line.";
-
-    }
+  }
+//passed
+function nowServing(katzDeliLine) {
+  if katzDeliLine[0] === undefined) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var firstInLine = katzDeliLine.shift()
+    return "Currently serving " + firstInLine + ".";
+  }
+}
