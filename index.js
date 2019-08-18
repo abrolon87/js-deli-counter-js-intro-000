13f8ba70576f5b7937cc9281d9eb1896c8c493d0
Amanda R <abrolon87@gmail.com>
2019-08-18 03:38:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return "Welcome, " + name + ". You are number " + (katzDeliLine.indexOf(name) + 1) + " in line."
-      You are ${i} in line`;
+  return "Welcome, " + name + ". You are number " + (katzDeliLine.indexOf(name) + 1) + " in line.";
+      
     }
